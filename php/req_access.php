0237a4297e556553324e36c7283a1df38d80b090
Blake_afk_del <blakegergely@gmail.com>
2026-06-18 22:08:48 -0400

    Finalized the requset acess page and all the required information. fixed logbook linking.

--- php/req_access.php
+++ php/req_access.php
@@ -11,7 +11,8 @@
             <li><b>First Name</b>: <?php echo $_POST['firstname']; ?></li>
             <li><b>Last Name</b>: <?php echo $_POST['lastname']; ?></li>
             <li><b>Who They Are </b>: <?php echo $_POST['who_are_you']; ?></li>
-            <li><b>involvement </b>: <?php echo $_POST['involvement']; ?></li>
+            <li><b>Email</b>: <?php echo $_POST['eamil']; ?></li>
+            <li><b>Birth date</b>: <?php echo $_POST['email']; ?></li>
         </ul>
         </b>
         <p><a href="../index.html"> Home Page</a></p>
