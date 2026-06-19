82e4d52e3e5cb4b5c3e54fd12891815ce29609e2
Blake_afk_del <blakegergely@gmail.com>
2026-06-18 21:47:47 -0400

    fixed  the php files and add a link to go back to the page

--- php/login.php
+++ php/login.php
@@ -11,5 +11,8 @@
             <li><b>username</b>: <?php echo $_POST['username']; ?></li>
             <li><b>password</b>: <?php echo $_POST['password']; ?></li>
         </ul>
+        </b>
+        <p><a href="../index.html"> Home Page</a></p>
+        <p>Copyright &copy; Owen K., Leighton E., Blake G.</p>
     </body>
 </html>
--- php/req_access.php
+++ php/req_access.php
@@ -8,10 +8,13 @@
         <p>Form submitted? <?php echo (int) $submitted; ?> </p>
         <p>your information is</p>
         <ul>
-            <li><b>First Name:</b>: <?php echo $_POST['firstname']; ?></li>
-            <li><b>Last Name:</b>: <?php echo $_POST['lastname']; ?></li>
-            <li><b>Who They Are: </b>: <?php echo $_POST['who_are_you']; ?></li>
-            <li><b>involvement: </b>: <?php echo $_POST['involvement']; ?></li>
+            <li><b>First Name</b>: <?php echo $_POST['firstname']; ?></li>
+            <li><b>Last Name</b>: <?php echo $_POST['lastname']; ?></li>
+            <li><b>Who They Are </b>: <?php echo $_POST['who_are_you']; ?></li>
+            <li><b>involvement </b>: <?php echo $_POST['involvement']; ?></li>
         </ul>
+        </b>
+        <p><a href="../index.html"> Home Page</a></p>
+        <p>Copyright &copy; Owen K., Leighton E., Blake G.</p>
     </body>
 </html>
